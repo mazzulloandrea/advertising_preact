c58d545ea9fd170682d86212569d4a8da501b05f
Andrea Mazzullo <andrea.mazzullo@spindox.it>
2022-08-01 15:42:14 +0200

    fix left

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -5,25 +5,26 @@ import Blur from '../../components/blur';
 import Quiz from '../../components/quiz';
 
 const Home = () => {
+
   function close() {
     const anim = document.getElementById("animation");
     if (anim.classList.value.includes('exit')) {
       return;
     } else {
       anim.classList.toggle(style.exit);
-      anim.onanimationend = (evt => evt.target.style.left = '-25vw')
+      anim.onanimationend = (evt => evt.target.style.left = '-100vw')
     }
   };
 
   return html`
     <div id="animation" class=${style.container}>
       <div class=${style.closeContainer}>
-        <${CloseButton} closeAnimation=${(evt) => close()} />
+        <${CloseButton} closeAnimation=${(evt)=> close()} />
       </div>
-      <div class=${style.blurSection} onClick=${() => window.open("http://www.google.it")}>
+      <div class=${style.blurSection} onClick=${()=> window.open("http://www.google.it")}>
         <${Blur} />
       </div>
-      <div class=${style.quizSection} onClick=${() => window.open("http://www.google.it")}>
+      <div class=${style.quizSection} onClick=${()=> window.open("http://www.google.it")}>
         <${Quiz} />
       </div>
     </div>
